945d7d2e3f394671c036840412c636dbc31aceb0
punitbudania <punitbudania88@gmail.com>
2020-11-22 15:09:55 +0530

    [punit]: Enter email ID UC3

--- src/samplePackage/EmailID.java
+++ src/samplePackage/EmailID.java
@@ -4,22 +4,21 @@ import java.util.Scanner;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-public class LastName 
+public class EmailID 
 {
-	public static void lastname()
+	public static void email()
 	{
 		Scanner firstname = new Scanner(System.in);
 		
-		String any = "^[A-Z][a-z]{2,}";
+		String any = "^([a-z]{3})([+_.-]?[0-9a-z]{3,})?([@][0-9a-z]{1,})([.][a-z]{2,})([.][a-z]{2,})?";
 		Pattern pattern = Pattern.compile(any, Pattern.MULTILINE);
 		Matcher matcher = pattern.matcher(firstname.next());
 		boolean found = false;
 		
 		while(matcher.find())
 		{
-			System.out.println("Enter EmailID:");
-			EmailID emailreg = new EmailID();
-			emailreg.email();
+			System.out.println("Enter MobileNo:");
+			
 			found = true;
 		}
 		
@@ -29,6 +28,3 @@ public class LastName
 		}
 	}
 }
-
-
-
