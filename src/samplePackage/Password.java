84b8eb30bc52c7464625c5f1b152a173eec795f3
punitbudania <punitbudania88@gmail.com>
2020-11-22 15:40:24 +0530

    [punit]: Registration complete

--- src/samplePackage/Password.java
+++ src/samplePackage/Password.java
@@ -4,22 +4,21 @@ import java.util.Scanner;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-public class MobileNo
+public class Password
 {
-	public static void mobile()
+	public static void pswd()
 	{
 		Scanner firstname = new Scanner(System.in);
 		
-		String any = "[0-9]{2}\\s[0-9]{10}";
+		String any = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&-+=()])(?=\\\\S+$).{8, 20}$";
 		Pattern pattern = Pattern.compile(any, Pattern.MULTILINE);
 		Matcher matcher = pattern.matcher(firstname.next());
 		boolean found = false;
 		
 		while(matcher.find())
 		{
-			System.out.println("Enter Password:");
-			Password paswrd = new Password();
-			paswrd.pswd();
+			System.out.println("Done");
+			
 			found = true;
 		}
 		
