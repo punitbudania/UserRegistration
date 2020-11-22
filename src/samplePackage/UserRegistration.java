20c9e4187bec39eb578cb85ae274168b108e22f7
punitbudania <punitbudania88@gmail.com>
2020-11-22 11:15:49 +0530

    [punit]: First Name UC1

---
+++ src/samplePackage/UserRegistration.java
@@ -0,0 +1,29 @@
+package samplePackage;
+
+import java.util.regex.*;
+import java.util.Scanner;
+
+public class UserRegistration 
+{
+	public static void main(String[] args)
+	{
+		Scanner firstname = new Scanner(System.in);
+		
+		String any = "^[A-Z][a-z]{2,}";
+		Pattern pattern = Pattern.compile(any, Pattern.MULTILINE);
+		System.out.println("Enter FirstName:");
+		Matcher matcher = pattern.matcher(firstname.next());
+		boolean found = false;
+		
+		while(matcher.find())
+		{
+			System.out.println("Valid");
+			found = true;
+		}
+		
+		if(!found)
+		{
+			System.out.println("Invalid");
+		}
+	}
+}
